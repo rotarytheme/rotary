11d4119118111a99dfb2b4969f380df805a30dc4
koolkatwebdesigns <merrill@koolkatwebdesigns.com>
2015-06-17 17:52:05 -0700

    fix conflict

--- includes/js/special_js.js
+++ includes/js/special_js.js
@@ -3,13 +3,13 @@ jQuery(document).ready(function($) {
     var bug_div = $('#acf-speaker_program_content');
     var bug_el = bug_div.find('textarea');
     var t_val;
-    if( bug_el.attr('class') != '' ){
+    if( bug_el.attr('class') !== '' ){
         bug_el.change(t_require_bug); t_require_bug();
     }
     function t_require_bug(){
         t_val = get_textfield_content();
         console.log( t_val );
-        if( t_val != '' ){
+        if( t_val !== '' ){
             bug_div.removeClass('required').addClass('require_bug');
         } else {
             bug_div.removeClass('require_bug').addClass('required');
@@ -23,8 +23,6 @@ jQuery(document).ready(function($) {
          return bug_div.val();
          }*/
     }
-<<<<<<< HEAD
+
 });
-=======
-});
->>>>>>> origin/master
+
