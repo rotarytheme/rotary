c7662ba4800c9e64e920e66b631a0f7e1b9dd0fd
Merrill Mayer <merrill@koolkatwebdesigns.com>
2016-01-15 18:53:42 -0800

    Add viewport meta tag

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <!--[if IE]>
